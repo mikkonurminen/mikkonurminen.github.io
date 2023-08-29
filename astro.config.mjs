3a5161f8f81967d821891a8b0950dfe28b3292ba
mikkonurminen <mmnurm@gmail.com>
2023-08-29 20:23:45 +0300

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mikkonurminen.github.io",
+  site: "https://mikkonurminen.com",
 })
